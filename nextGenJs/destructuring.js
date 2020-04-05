60ff3b0cf9c69147b637da45ee07ed16ccb4f28e
Deepjyoti Borah <deepborah86@gmail.com>
2020-04-06 00:09:47 +0530

    Adding files

---
+++ nextGenJs/destructuring.js
@@ -0,0 +1,6 @@
+//  Destructuring easily extract array element od object properties
+// and store them in variable
+
+const numbers = [1,2,3];
+[num1, num2] = numbers;
+console.log(num1 + " ", num2); // 1 2

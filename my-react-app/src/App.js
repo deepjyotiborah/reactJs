c9674528e42d6fd43cef2ec3e66506c6bdf4d137
Deepjyoti Borah <deepborah86@gmail.com>
2020-04-11 01:16:57 +0530

    Adding files

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1> It's a react app</h1>
-      <Person name="Deep" age="34"/>
-      <Person name="urmi" age="30"> MY hobbies : Watching Movies </Person>
-      <Person name="Adam" age="35"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Deep", age: "34"},
+      {name: "urmi", age: "30"},
+      {name: "Adam", age: "35"}
+  ]};
+
+  render() {
+    return (
+      <div className="App">
+        <h1> It's a react app</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> MY hobbies : Watching Movies </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- my-react-app/src/Person/Person.js
+++ my-react-app/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const person = (props) => {
     return (
         <div>
-            <h2> I am {props.name} and I am {props.age} years old</h2>
+            <h2> I am {props.name} and I am {props.age} years old</h2>  
             <p>{props.children}</p>
         </div>
     )

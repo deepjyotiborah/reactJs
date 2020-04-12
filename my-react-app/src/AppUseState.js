b03a14cdd3a7aad0fe5f757ca6979f03fbbe42b1
Deepjyoti Borah <deepborah86@gmail.com>
2020-04-12 23:48:19 +0530

    Adding files

--- my-react-app/src/AppUseState.js
+++ my-react-app/src/AppUseState.js
@@ -5,56 +5,98 @@ import Person from './Person/Person.js'
 const AppUse = props => {
     const [personsState, setPersons] = useState({
         persons: [
-            { name: "Deep", age: "34" },
-            { name: "urmi", age: "30" },
-            { name: "Adam", age: "35" }
+            { id:'1',name: "Deep", age: "34" },
+            { id:'2',name: "urmi", age: "30" },
+            { id:'3',name: "Adam", age: "35" }
         ],
-        otherState: 'Some other state'
+        otherState: 'Some other state',
+        showPersons: true
     });
 
-    const switchHandler = (newName) => {
-        setPersons({
-            persons: [
-                { name: newName, age: "34" },
-                { name: "urmi", age: "30" },
-                { name: "Adam", age: "40" }
-            ]
-        })
-    };
+    // const switchHandler = (newName) => {
+    //     setPersons({
+    //         persons: [
+    //             { id:'1',name: newName, age: "34" },
+    //             { id:'2',name: "urmi", age: "30" },
+    //             { id:'3',name: "Adam", age: "40" }
+    //         ],
+    //         otherState: personsState.otherState,
+    //         showPersons: personsState.showPersons
+    //     })
+    // };
 
-    const nameChangeHandler = (event) => {
+    const nameChangeHandler = (event, id) => {
+        const personIdx = personsState.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const p = personsState.persons.find(pp => {
+            return pp.id === id;
+        })
+
+        const person = {
+            ...personsState.persons[personIdx]
+        };
+
+        person.name = event.target.value;
+        
+        const persons = [...personsState.persons];
+        persons[personIdx] = person;
+
         setPersons({
-            persons: [
-                { name: "Deep", age: "34" },
-                { name: event.target.value, age: "29" },
-                { name: "Adam", age: "37" }
-            ]
+            persons: persons,
+            otherState: personsState.otherState,
+            showPersons: personsState.showPersons
         })
     }
 
-    const style =   {
-        backgroundColor: 'White', 
+    const togglePersons = () => {
+        const doesShow = personsState.showPersons;
+        setPersons({
+            persons: personsState.persons,
+            otherState: personsState.otherState,
+            showPersons: !doesShow
+        });
+    }
+
+    const deletePersonHandler = (index) => {
+        //const persons = personsState.persons.slice();
+        const persons = [...personsState.persons];
+        persons.splice(index, 1);
+        setPersons({
+            persons: persons,
+            otherState: personsState.otherState,
+            showPersons: personsState.showPersons
+        });
+    }
+
+    const style = {
+        backgroundColor: 'White',
         font: 'inherit',
         border: '1px solid blue',
-        padding:'8px',
-        cursoe:'pointer' 
+        padding: '8px',
+        cursoe: 'pointer'
     }
+
     return (
         <div className="App">
             <h1> It's a react app</h1>
             <button
-                style={style}
-                onClick={switchHandler.bind(this, "DeepChanged")} value="Switch" >Switch Name
+                style={style} onClick={togglePersons} value="Switch" > Toggle Persons
             </button>
-            <Person 
-                name={personsState.persons[0].name} age={personsState.persons[0].age}
-            />
-            <Person click= {() => switchHandler('MyNewName')} changed = {nameChangeHandler}
-                name={personsState.persons[1].name} age={personsState.persons[1].age}> MY hobbies : Watching Movies
-            </Person>
-            <Person
-                name={personsState.persons[2].name} age={personsState.persons[2].age}
-            />
+            {personsState.showPersons ?
+
+                <div>
+                    {
+                        personsState.persons.map((person, index) => {
+                            return <Person click={() => deletePersonHandler(index)} 
+                                changed={(event) => nameChangeHandler(event, person.id)}
+                                name={person.name} age={person.age} key={person.id}>
+                            </Person>
+                        })
+                    }
+                </div> : null
+            }
         </div>
     );
 

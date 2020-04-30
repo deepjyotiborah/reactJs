91588f57f2a1c2b3a67584a3b42a879a08b1c582
Deepjyoti Borah <deepborah86@gmail.com>
2020-04-30 12:20:30 +0530

    Changes for lifecycle events.

--- my-react-app/src/components/Persons/Person/Person.js
+++ my-react-app/src/components/Persons/Person/Person.js
@@ -1,31 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 //  import Person from './Person.css'
 //import Radium from 'radium'
-import styled from 'styled-components'
+import classes from './Person.module.css'
+import { isClassExpression } from '@babel/types'
 
-const person = (props) => {
-   
-    console.log('[Person.js] called.')
-    const StyledDiv = styled.div`
-        width: 60%;
-        margin: auto;
-        border: 1px solid #eee;
-        box-shadow: 0 2px 3px #ccc;
-        padding: 16px;
-        text-align: center;
-    
-        // @media (min-width: 500px) {
-        //     width: 450px;
-        // }
-    `;
+class Person extends Component {
 
-    return (
-       <StyledDiv>
-           <p onClick = {props.click}> I am {props.name} and I am {props.age} years old</p>  
-           <p>{props.children}</p> 
-           <input type='text' onChange={props.changed} value={props.name}/>
-       </StyledDiv>
-    )
+    render() {
+        console.log('[Person.js] called.')
+        return (
+           <div className={classes.Person}>
+               <p onClick = {this.props.click}> I am {this.props.name} and I am {this.props.age} years old</p>  
+               <p>{this.props.children}</p> 
+               <input type='text' onChange={this.props.changed} value={this.props.name}/>
+           </div>
+        )
+    }
 }
 
-export default person;
+export default Person;
--- my-react-app/src/components/Persons/Persons.js
+++ my-react-app/src/components/Persons/Persons.js
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Person from './Person/Person'
 import ErrorBoundary from '../ErrorBoundary/ErroBoundary'
 
-const persons = (props) => {
-    console.log('[Persons.js] rendering...')
-    return props.persons.map((person, index) => {
-        return <ErrorBoundary key={person.id}>
-           
-            <Person
-                click={() => props.click(index)}
-                changed={(event) => props.changed(event, person.id)}
-                name={person.name}
-                age={person.age}
-                key={person.id}>
-            </Person>
-        </ErrorBoundary>
-    });
+class Persons extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate called.')
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevSTate) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate called');
+        return {message: 'Snapshot'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate called.')
+        console.log(snapshot);
+    }
+    
+    render() {
+        console.log('[Persons.js] rendering...')
+
+        return this.props.persons.map((person, index) => {
+            return <ErrorBoundary key={person.id}>
+               
+                <Person
+                    click={() => this.props.click(index)}
+                    changed={(event) => this.props.changed(event, person.id)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}>
+                </Person>
+            </ErrorBoundary>
+        });
+    }
 }
 
 
 
-export default persons;
+export default Persons;

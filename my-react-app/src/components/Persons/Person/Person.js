a56da2812176f77c3e383926f6e187dbbbd7e6e1
Deepjyoti Borah <deepborah86@gmail.com>
2020-05-02 13:16:55 +0530

    Passing props to HOC.

--- my-react-app/src/components/Persons/Person/Person.js
+++ my-react-app/src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import classes from './Person.module.css'
+import Aux from '../../../hoc/Aux';
+import withClasses from '../../../hoc/withClasses';
 
 class Person extends Component {
 
     render() {
         console.log('[Person.js] called.')
         return (
-           <div className={classes.Person}>
-               <p onClick = {this.props.click}> I am {this.props.name} and I am {this.props.age} years old</p>  
-               <p>{this.props.children}</p> 
-               <input type='text' onChange={this.props.changed} value={this.props.name}/>
-           </div>
+            <Aux>
+                <p onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type='text' onChange={this.props.changed} value={this.props.name} />
+            </Aux>
         )
     }
 }
 
-export default Person;
+export default withClasses(Person, classes.Person);
--- my-react-app/src/hoc/WithClasses.js
+++ my-react-app/src/hoc/WithClasses.js
@@ -4,7 +4,7 @@ import React from 'react';
 const withClasses = (WrappeComponent, className) => {
     return props => (
         <div className={className}>
-            <WrappeComponent />
+            <WrappeComponent {...props} />
         </div>
     );
 } 

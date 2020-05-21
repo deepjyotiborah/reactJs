b890b0ee47fb8a53418318b43227f81c5cdbaffd
deepjyotiborah <deepborah86@gmail.com>
2020-05-21 11:41:56 +0530

    Adding cancel & continue button functionality in order summery

--- burger-builder-app/src/components/OrderSummary/OrderSummary.js
+++ burger-builder-app/src/components/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Button from '../../components/UI/Button/Button';
 
 const OrderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -17,6 +18,8 @@ const OrderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
+            <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
         </Aux>
     )
 }

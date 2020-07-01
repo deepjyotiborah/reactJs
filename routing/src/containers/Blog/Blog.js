e97b14cf8acde1ed7bd3eef2e96b4e1ef0ccc2ab
Deepjyoti Borah <deepborah86@gmail.com>
2020-07-01 16:19:11 +0530

    Adding links

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from '../Posts/Posts'
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
+import NewPost from '../NewPost/NewPost'
 
 class Blog extends Component {
     
-    
-
     render () {
-        
-
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                search: '?searchParam=test',
+                                hash: '#submit'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts}/>
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         );
     }
